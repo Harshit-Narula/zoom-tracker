73000d19cf3ce43ceac3485ec5d5afe55a0915d7
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-10-26 17:35:19 +0530

    backend

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const fileUpload = require('express-fileupload');
 var fs = require('fs');
 var parse = require('csv-parse');
 var mysql = require('mysql');
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -11,12 +13,22 @@ var con = mysql.createConnection({
     database: "csv_data"
 });
 
+// allow cors for all requests *
+var cors = require('cors');
+
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
 const app = express();
+var corsOptions = {
+    origin: "*"
+};
+app.use(cors(corsOptions));
+
+// make public folder 
+app.use(express.static('public'));
 
 app.use(express.json())
 app.use(fileUpload());
@@ -24,26 +36,46 @@ app.use(express.urlencoded({ extended: true }))
 
 // Upload Endpoint
 app.post('/upload', (req, res) => {
-    if (req.files === null) return res.status(400).json({ msg: 'No file uploaded' });
+    if (req.files === null) return res.status(200).json({ success: 0, msg: 'No file uploaded' });
+    let file = req.files.file;
     var csvData = [];
-
-    fs.createReadStream(`${__dirname}/client/public/uploads/${file.name}`)
-        .pipe(parse({ delimiter: ',' }))
-        .on('data', function (csvrow) {
-            // console.log(csvrow);
-            //do something with csvrow
-            var sql = `INSERT INTO csv_data VALUES ('${csvrow[0]}', '${csvrow[1]}','${csvrow[3]}', '${csvrow[4]}','${csvrow[5]}', '${csvrow[6]}')`;
-            con.query(sql, function (err, result) {
-                if (err) throw err;
-                console.log(" record inserted");
+    let fileName = `${__dirname}/client/public/uploads/${file.name}`
+    file.mv(fileName, err => {
+        if (err) {
+            console.log(err);
+            return res.status(200).json({
+                success: 0,
+                msg: 'Error uploading file'
             });
-            // console.log(csvrow);
-        })
-        .on('end', function () {
-            console.log('done');
-            res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-        });
+        } else {
+            fs.createReadStream(fileName)
+                .pipe(parse({ delimiter: ',' }))
+                .on('data', function (csvrow) {
+                    // console.log(csvrow);
+                    //do something with csvrow
+                    var sql = `INSERT INTO csv_data(uuid ,name, email, count, date, date_of_webinar, image_url, batch_details, zoom_link) VALUES ('${uuidv4()}','${csvrow[0]}', '${csvrow[1]}','${csvrow[2]}', now(),'${csvrow[4]}', '${csvrow[5]}', '${csvrow[6]}', '${csvrow[7]}')`;
+                    console.log(sql);
+                    con.query(sql, function (err, result) {
+                        if (err) throw err;
+                        console.log(" record inserted");
+                    });
+                    // console.log(csvrow);
+                })
+                .on('end', function () {
+                    console.log('done');
+                    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+                })
+                .on('error', function (err) {
+                    console.log(err.message);
+                    res.status(500).json({ success: 0, error: err.message });
+                });
+        }
+    });
 });
+
+app.get("/", (req, res) => {
+    res.json({ msg: "backend is running on port 5000" })
+})
 
 app.post('/getAll', (req, res) => {
     const date = req.body.date;
@@ -72,16 +104,19 @@ app.get("/getWebinarDetails", (req, res) => {
     })
 })
 app.post("/enterZoomLink", (req, res) => {
-    var sql = `INSERT INTO csv_data(zoom_link) VALUES ('${req.body.link}') where date = now()`;
+    var sql = `update csv_data set zoom_link = ${req.body.link} where date = now()`;
     con.query(sql, (err, result) => {
-        if (err) res.json({
-            success: false,
-            error: err.message
-        })
-        res.json({
-            success: true,
-            result
-        })
+        if (err) {
+            res.json({
+                success: false,
+                error: err.message
+            })
+        } else {
+            res.json({
+                success: true,
+                result
+            })
+        }
     })
 })
 
