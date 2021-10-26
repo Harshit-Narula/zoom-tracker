9785fea1b6bad8d2af281246c67b324313ac9f95
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-10-26 19:04:57 +0530

    a

--- server.js
+++ server.js
@@ -13,14 +13,12 @@ var con = mysql.createConnection({
     database: "csv_data"
 });
 
-// allow cors for all requests *
 var cors = require('cors');
-
-
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
+
 const app = express();
 var corsOptions = {
     origin: "*"
@@ -73,9 +71,52 @@ app.post('/upload', (req, res) => {
     });
 });
 
-app.get("/", (req, res) => {
-    res.json({ msg: "backend is running on port 5000" })
+app.get("/count", (req, res) => {
+    var sql = `SELECT count(*) as total FROM csv_data`;
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(" record inserted");
+        res.json(result);
+    });
 })
+
+app.post("/update", (req, res) => {
+    var sql = `update csv_data set page_open_count=page_open_count+1 where uuid='${req.body.uuid}'`;
+    con.query(sql, function (err, result) {
+        if (err) {
+            res.status(200).json({
+                success: 0,
+                error: err.message
+            });
+        } else {
+            res.json({
+                success: 1,
+                result: result
+            });
+        }
+    });
+})
+
+app.post("/updateCount", (req, res) => {
+    var sql = `update csv_data set count=count+1 where uuid='${req.body.uuid}'`;
+    con.query(sql, function (err, result) {
+        if (err) {
+            res.status(200).json({
+                success: 0,
+                error: err.message
+            });
+            console.log(error);
+        } else {
+            console.log(result);
+            res.json({
+                success: 1,
+                result: result
+            });
+        }
+    });
+})
+
+
 
 app.post('/getAll', (req, res) => {
     const date = req.body.date;
@@ -90,21 +131,44 @@ app.post('/getAll', (req, res) => {
     })
 });
 
-app.get("/getWebinarDetails", (req, res) => {
-    var sql = `SELECT DISTINCT date ,image_url, date_of_webinar, batch_details where date = ${req.body.date}`;
+
+// app.get("/getWebinarDetails", (req, res) => {
+//     var sql = `SELECT DISTINCT date ,image_url, date_of_webinar, batch_details where date = ${req.body.date}`;
+//     con.query(sql, (err, result) => {
+//         if (err) {
+//             res.json({
+//                 success: false,
+//                 error: err.message
+//             })
+//         } else {
+//             res.json({
+//                 success: true,
+//                 result
+//             })
+//         }
+//     })
+// })
+app.post("/enterZoomLink", (req, res) => {
+    var sql = `update csv_data set zoom_link = '${req.body.link}' where date = current_date()`;
+    console.log(sql);
     con.query(sql, (err, result) => {
-        if (err) res.json({
-            success: false,
-            error: err.message
-        })
-        res.json({
-            success: true,
-            result
-        })
+        if (err) {
+            res.json({
+                success: false,
+                error: err.message
+            })
+            console.log(err);
+        } else {
+            res.json({
+                success: true,
+                result
+            })
+        }
     })
 })
-app.post("/enterZoomLink", (req, res) => {
-    var sql = `update csv_data set zoom_link = ${req.body.link} where date = now()`;
+
+app.get("/getWebinarDetails", (req, res) => {
+    var sql = `SELECT zoom_link,date_of_webinar, time_of_webinar, details from csv_data where date = current_date() limit 1`;
     con.query(sql, (err, result) => {
         if (err) {
             res.json({
@@ -120,4 +184,24 @@ app.post("/enterZoomLink", (req, res) => {
     })
 })
 
+app.get("/getZoomLink/:email", (req, res) => {
+    let email = req.params.email;
+    var sql = `SELECT uuid from csv_data where email = '${email}'`;
+    con.query(sql, (err, result) => {
+        if (err) {
+            res.json({
+                success: false,
+                error: err.message
+            })
+        } else {
+            res.send(
+                "www.pepcoding.com/webinar/" + result[0].uuid
+            )
+        }
+    })
+})
+
+app.get("/", (req, res) => {
+    res.json({ msg: "backend is running on port 5000" })
+})
 app.listen(5000, () => console.log('Server Started...'));
