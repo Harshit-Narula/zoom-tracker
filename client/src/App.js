5589171ea700dd7a61a615f94a1311ee83f2c007
Arun <arun8778451315@gmail.com>
2021-10-26 11:26:22 +0530

    Frontend Update

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,19 @@ import './App.css';
 import Table from './components/Table';
 
 const App = () => {
-  const [data,setData] = useState([{name:"Udai",phone:"9999999999"},{name:"Udai",phone:"9999999999"},{name:"Udai",phone:"9999999999"},{name:"Udai",phone:"9999999999"}])
-  useEffect(() => {
+    const [data, setData] = useState([{ name: "Udai", phone: "9999999999" }, { name: "Udai", phone: "9999999999" }, { name: "Udai", phone: "9999999999" }, { name: "Udai", phone: "9999999999" }])
+    useEffect(() => {
 
-  }, [])
+    }, [])
 
-  return (
-    <div className='container mt-4'>
-      <h4 className='display-4 text-center mb-4'>
-        <i className='fas fa-video' /> Zoom Attendance
-      </h4>
+    return (
+        <div className='container mt-4'>
+            <h4 className='display-4 text-center mb-4'>
+                <i className='fas fa-video' /> Zoom Attendance
+            </h4>
 
-      <Table data={data}/>
-    </div>)
+            <Table data={data} />
+        </div>)
 };
 
 export default App;
--- server.js
+++ server.js
@@ -6,16 +6,16 @@ var mysql = require('mysql');
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "csv_data"
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "csv_data"
 });
 
 
 con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
+    if (err) throw err;
+    console.log("Connected!");
 });
 const app = express();
 
@@ -24,35 +24,35 @@ app.use(fileUpload());
 
 // Upload Endpoint
 app.post('/upload', (req, res) => {
-  if (req.files === null) {
-    return res.status(400).json({ msg: 'No file uploaded' });
-  }
-
-  const file = req.files.file;
-
-  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
+    if (req.files === null) {
+        return res.status(400).json({ msg: 'No file uploaded' });
     }
-    var csvData = [];
-    fs.createReadStream(`${__dirname}/client/public/uploads/${file.name}`)
-      .pipe(parse({ delimiter: ',' }))
-      .on('data', function (csvrow) {
-        console.log(csvrow);
-        //do something with csvrow
-        var sql = `INSERT INTO csv_data (name, email) VALUES ( ${csvrow[0]}, ${csvrow[1]})`;
-        con.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log(" record inserted");
-        });
-        console.log(csvrow);
-      })
-      .on('end', function () {
-        console.log('done');
-        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-      });
-  });
+
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+        var csvData = [];
+        fs.createReadStream(`${__dirname}/client/public/uploads/${file.name}`)
+            .pipe(parse({ delimiter: ',' }))
+            .on('data', function (csvrow) {
+                console.log(csvrow);
+                //do something with csvrow
+                var sql = `INSERT INTO csv_data (name, email) VALUES ( ${csvrow[0]}, ${csvrow[1]})`;
+                con.query(sql, function (err, result) {
+                    if (err) throw err;
+                    console.log(" record inserted");
+                });
+                console.log(csvrow);
+            })
+            .on('end', function () {
+                console.log('done');
+                res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+            });
+    });
 });
 
 app.listen(5000, () => console.log('Server Started...'));
